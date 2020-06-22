1e7fea4b95bde0cbcb6a4e075cd7f52206ca9720
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-06-23 01:37:51 +0200

    Make file-store-fetcher-service actually fetch and serve the files

--- src/main/java/no/responseweb/imagearchive/filestorefetcherservice/web/controllers/FileStoreFetcherController.java
+++ src/main/java/no/responseweb/imagearchive/filestorefetcherservice/web/controllers/FileStoreFetcherController.java
@@ -6,11 +6,19 @@ import no.responseweb.imagearchive.filestorefetcherservice.services.FileInfoServ
 import no.responseweb.imagearchive.model.FileItemDto;
 import no.responseweb.imagearchive.model.FilePathDto;
 import no.responseweb.imagearchive.model.FileStoreDto;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @Slf4j
@@ -21,13 +29,32 @@ public class FileStoreFetcherController {
     private final FileInfoService fileInfoService;
 
     @GetMapping("api/v1/fetchFile/{fileItemId}")
-    public Object getOriginalFile(@PathVariable UUID fileItemId) {
+    public Object getOriginalFile(@PathVariable UUID fileItemId) throws IOException {
 
         FileItemDto fileItemDto = fileInfoService.getFileItem(fileItemId);
         FilePathDto filePathDto = fileInfoService.getFilePath(fileItemDto.getFileStorePathId());
         FileStoreDto fileStoreDto = fileInfoService.getFileStore(filePathDto.getFileStoreId());
 
-        return fileStoreDto.getLocalBaseUri() + File.separator + filePathDto.getRelativePath() + File.separator + fileItemDto.getFilename();
+        String s = fileStoreDto.getLocalBaseUri() + File.separator + filePathDto.getRelativePath() + File.separator + fileItemDto.getFilename();
+        Path path = Paths.get(s);
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        String detectedMimeType = Files.probeContentType(path);
+        MediaType detectedMediaType = MediaType.parseMediaType(detectedMimeType);
+        log.info("Detected Mime Type: {}. Detected Media Type: {}", detectedMimeType, detectedMediaType);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION,  "inline; filename=\"" +fileItemDto.getFilename()+ "\"");
+        //noinspection SpellCheckingInspection
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(fileItemDto.getSize())
+                .contentType(detectedMediaType)
+                .body(resource);
     }
 
 }
